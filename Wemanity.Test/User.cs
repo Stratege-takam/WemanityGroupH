f1fddd6ff68ead8c5affb3dc450a2e882ec401e1
Danick Takam <213346@supinfo.com>
2019-12-17 18:34:27 +0100

    Ajoutez des fichiers projet.

---
+++ Wemanity.Test/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wemanity.Test
+{
+   public class User
+    {
+        public int MyProperty { get; set; }
+    }
+}
---
+++ WemanityGroupH/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WemanityGroupH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
