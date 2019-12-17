3853bd497c413c4ff53ceb148321820ec28e56f6
Danick Takam <213346@supinfo.com>
2019-12-17 19:23:05 +0100

    added userservice

---
+++ WemanityGroupH/Services/UserService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WemanityGroupH.Models;
+
+namespace WemanityGroupH.Services
+{
+   public class UserService
+    {
+        public List<User> Users { get; set; }
+        public UserService()
+        {
+
+                Users = new List<User>()
+                {
+                    new User()
+                    {
+                         Lastname ="Doe",
+                         Firstname = "John",
+                         BirthDate = new DateTime(1982,10,8),
+                         Mail = "pkriwin@wemanity.com"
+                    },
+                     new User()
+                    {
+                         Lastname ="Takam",
+                         Firstname = "Danick",
+                         BirthDate = new DateTime(1979,12,12),
+                         Mail = "213346@supinfo.com"
+                    },
+                      new User()
+                    {
+                         Lastname ="Te",
+                         Firstname = "Emmanuelle",
+                         BirthDate = new DateTime(2005,2,31),
+                         Mail = "297192@supinfo.com"
+                    }
+                };
+        }
+        public void ReadFromFileAsync()
+        {
+            
+            return null;
+        }
+    }
+}
