5d77fcb9c4a0760af4add69358f3a41668bfa70a
Danick Takam <213346@supinfo.com>
2019-12-17 19:11:56 +0100

    Added user model

---
+++ WemanityGroupH/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace WemanityGroupH.Models
+{
+  public  class User
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Mail { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+    }
+}
