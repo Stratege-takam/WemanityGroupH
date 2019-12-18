239caf6606c67898a4c3075900e92a3673dfac67
Danick Takam <213346@supinfo.com>
2019-12-18 12:47:00 +0100

    added remeber

--- WemanityGroupH/Services/BirthdayGreeterService.cs
+++ WemanityGroupH/Services/BirthdayGreeterService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using WemanityGroupH.Models;
 
 namespace WemanityGroupH.Services
 {
@@ -10,18 +12,49 @@ namespace WemanityGroupH.Services
         public MailingService MailingService { get; set; }
 
         private const string _mailTemplate = "Happy Birthday, dear {firstname}!";
+        private const string _mailTemplateRemember = "Dear <firstname>,<p>Today is <allfullname>'s birthday. <br/> Don't forget to send them a message !</p>";
         private const string _subject = "Happy birthday!";
 
        public void GreetBirthdays()
-        {
-            foreach (var user in UserService.Users)
+       {
+            var greetUsers = UserService.Users.Where(user => IsGreetBirthdays(user)).ToList();
+            foreach (var user in greetUsers)
             {
-                if (user.BirthDate.Date.Equals( DateTime.Now.Date))
-                {
-                    MailingService.SendMail($"{user.Firstname} {user.Lastname}", user.Mail,
-                        _subject, _mailTemplate.Replace("{firstname}", user.Firstname));
-                }
+                MailingService.SendMail($"{user.Firstname} {user.Lastname}", user.Mail,
+                         _subject, _mailTemplate.Replace("{firstname}", user.Firstname));
             }
+
+            RememberBirthdays(greetUsers);
+       }
+
+        public void RememberBirthdays(List<User> users)
+        {
+            var ListFullname = users?.Select((user, i) => 
+                    i == users.Count -1 ?
+                        $"{user.Firstname} {user.Lastname}" : 
+                    i == users.Count - 2 ?
+                        $"{user.Firstname} {user.Lastname} and " :
+                        $"{user.Firstname} {user.Lastname}, " 
+                );
+            if (ListFullname !=null)
+            {
+                MailingService.SendMail(MailingService.FromName, MailingService.FromEmail,
+                         _subject, _mailTemplateRemember
+                         .Replace("<firstname>", MailingService.FromName)
+                         .Replace("<allfullname>", string.Join("",ListFullname)));
+            }
+        }
+
+       public bool IsGreetBirthdays(User user)
+       {
+            return (user.BirthDate.Date.Equals(DateTime.Now.Date)
+                        && !IsLastDayOfFebruary(user.BirthDate.Date)) ||
+                        IsLastDayOfFebruary(user.BirthDate.Date);
+       }
+
+        private bool IsLastDayOfFebruary(DateTime date)
+        {
+            return date.Month == 2 && date.Day == 29 &&  DateTime.Now.Day == 28;
         }
     }
 }
